a7b0175976be54988f7008554ad85d034f1e7b43
ArtB <89324748589@mail.ru>
2023-09-23 09:07:31 +0500

    Java app for a toy store

--- src/Main.java
+++ src/Main.java
@@ -9,40 +9,30 @@ import view.View;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем экземпляр ToyCollection с типом элементов Toy и присваиваем его переменной toyCollection.
         ToyCollection<Toy> toyCollection = new ToyCollection<>();
         
-        // Создаем экземпляр PrizeToyStore с типом элементов Toy и присваиваем его переменной prizeToyInventory.
         PrizeToyStore<Toy> prizeToyInventory = new PrizeToyStore<>();
 
-        // Создаем экземпляры Toy с указанными параметрами и присваиваем их переменным toy1, toy2, toy3 и toy4.
-        Toy toy1 = new Toy("Тигр", 15, 10.4);
-        Toy toy2 = new Toy("Лев", 15, 7);
-        Toy toy3 = new Toy("Зебра", 15, 20);
+        Toy toy1 = new Toy("Тигр", 15, 10.4); // Создаем экземпляры Toy
+        Toy toy2 = new Toy("Лев", 15, 7);     // с указанными параметрами
+        Toy toy3 = new Toy("Зебра", 15, 20);  // и присваиваем их переменным
         Toy toy4 = new Toy("Слон", 15, 4);
 
-        // Добавляем созданные экземпляры Toy в toyCollection.
-        toyCollection.add(toy1);
+        toyCollection.add(toy1); // Добавляем созданные экземпляры Toy в toyCollection.
         toyCollection.add(toy2);
         toyCollection.add(toy3);
         toyCollection.add(toy4);
 
-        // Создаем экземпляр FileManager и присваиваем его переменной fileHandler.
-        FileManager fileHandler = new FileManager();
+        FileManager fileHandler = new FileManager(); // Добавляем созданные экземпляры Toy в toyCollection.
 
-        // Создаем экземпляр Service с переданным fileHandler и присваиваем его переменной service.
         Service service = new Service(fileHandler);
 
-        // Устанавливаем fileHandler в service с помощью метода setFileIo.
-        service.setFileIo(fileHandler);
+        service.setFileIo(fileHandler); // fileHandler в service с помощью метода setFileIo.
 
-        // Создаем экземпляр Console и присваиваем его переменной console.
         Console console = new Console();
 
-        // Создаем экземпляр View и присваиваем его переменной view.
-        View view = new View();
+        View view = new View(); // Экземпляр View и присваиваем его переменной view.
 
-        // Устанавливаем toyCollection, prizeToyInventory и fileHandler в console и view с помощью соответствующих методов.
         console.setToyCollection(toyCollection);
         console.setPrizeToyStore(prizeToyInventory);
         console.setFileHandler(fileHandler);
@@ -50,8 +40,7 @@ public class Main {
         view.setToyCollection(toyCollection);
         view.setPrizeToyStore(prizeToyInventory);
 
-        // Запускаем метод start() у console для начала работы терминального интерфейса.
-        console.start();
+        console.start(); // Запускаем метода start() для начала работы интерфейса
     }
 }
 
